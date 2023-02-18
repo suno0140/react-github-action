e52b783531c89fb8e8d34c6ae5d72a68a2a6a1aa
suno Choi <vlfxhd@naver.com>
2023-02-19 03:12:44 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -9,24 +9,27 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{count}</h3>
-        <button
-          data-testid="minus-button"
-          disabled={disabled}
-          onClick={() => {
-            setCount((prev) => prev - 1);
-          }}
-        >
-          -
-        </button>
-        <button
-          data-testid="plus-button"
-          disabled={disabled}
-          onClick={() => {
-            setCount((prev) => prev + 1);
-          }}
-        >
-          +
-        </button>
+        <div>
+          <button
+            data-testid="plus-button"
+            disabled={disabled}
+            onClick={() => {
+              setCount((prev) => prev + 1);
+            }}
+          >
+            +
+          </button>
+          <button
+            data-testid="minus-button"
+            disabled={disabled}
+            onClick={() => {
+              setCount((prev) => prev - 1);
+            }}
+          >
+            -
+          </button>
+        </div>
+
         <div>
           <button
             style={{ backgroundColor: "blue" }}
